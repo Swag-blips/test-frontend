115bcb8662fdda8a40bb398f2f2591b6d76bcf7f
Bakrin Olasupo <coderblip@gmail.com>
2026-04-15 12:57:56 +0100

    fix:remove setup node events

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,9 +5,7 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173",
     supportFile: false,
-    setupNodeEvents(_on, _config) {
-      // implement node event listeners here
-    },
+
     viewportWidth: 1280,
     viewportHeight: 720,
     video: false,
